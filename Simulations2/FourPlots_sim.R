e74b8332511c04c1c89aab4613f646e831082ab7
Aaron Lun <alun@wehi.edu.au>
2017-10-12 15:32:46 +0100

    Reduced redundancy, improved clarity of simulation code.

--- Simulations2/FourPlots_sim.R
+++ Simulations2/FourPlots_sim.R
@@ -1,82 +1,82 @@
-#checked
+# This script compares the performance of various batch correction methods.
+
 library(scran)
-require(WGCNA)
+require(Rtsne)
+library(limma)
+library(sva)
 library(scales)
 
-setwd("/Users/laleh/projects/deposit2/Simulations2/")
 load("Sim.RData")
+dir.create("figs", showWarnings=FALSE)
 
-raw.all<-cbind(B1,B2)
-clustlabels<-cbind(clust1,clust2)
-colnames(raw.all)<-c(rep(1,dim(B1)[2]),rep(2,dim(B2)[2]))
-require(Rtsne)
-par(mfrow=c(1,1))
+plotFUN <- function(fname, Y, batch.id, cols, xlab="tSNE 1", ylab="tSNE 2", ...) {
+    png(fname, width=900, height=700)
+    par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
+    plot(Y[,1], Y[,2], 
+         pch=c(16, 2)[batch.id],
+         cex=c(2.5, 3.5)[batch.id],
+         col=alpha(cols, 0.6),
+         xlab=xlab, ylab=ylab, ...)
+    dev.off()
+}
 
-all.dists2.unc <- as.matrix(dist(t(raw.all)))
-set.seed(0)
-tsne.unc<-Rtsne(all.dists2.unc, is_distance=TRUE)#, perplexity = 0.9)
-########################
-N=1000
-setwd("/Users/laleh/projects/deposit2/Simulations2/figs")
-########################
+##########################################
+# Running through all of the methods.
 
-png(file="unc.png",width=900,height=700)
-par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
-plot(tsne.unc$Y[1:N,1],tsne.unc$Y[1:N,2], pch=16,cex=2.5,col=alpha((clustlabels[1:N]),0.6),main="Uncorrected",xlim=c(-35,30),ylim=c(-40,45),xlab="tSNE 1",ylab="tSNE 2")
-points(tsne.unc$Y[(N+1):(2*N),1],tsne.unc$Y[(N+1):(2*N),2], pch=2,cex=3.5,col=(clustlabels[(N+1):(2*N)]))
-dev.off()
+for (easy in c(FALSE, TRUE)) {
+    if (easy) {
+        B2 <- B2ii
+        clust2 <- clust2ii
+        prefix <- "easy_"
+    } else {
+        B2 <- B2i
+        clust2 <- clust2i
+        prefix <- ""
+    }
 
-#############MNN
-Xmnn<-mnnCorrect(B1,B2,inquiry.genes=NULL, hvg.genes=NULL, k=20, sigma=1, cos.norm=FALSE,svd.dim=2)
+    # Uncorrected.
+    raw.all <- cbind(B1, B2)
+    clust.cols <- c(clust1, clust2)
+    batch.id <- rep(1:2, c(ncol(B1), ncol(B2)))
 
-corre<-cbind(Xmnn$corrected[[1]],Xmnn$corrected[[2]])
-all.dists2.c <- as.matrix(dist(t(corre)))
+    all.dists2.unc <- as.matrix(dist(t(raw.all)))
+    set.seed(0)
+    tsne.unc <- Rtsne(all.dists2.unc, is_distance=TRUE)#, perplexity = 0.9)
+    plotFUN(paste0("figs/", prefix, "unc.png"), Y=tsne.unc$Y, batch.id=batch.id, cols=clust.cols, main="Uncorrected",xlim=c(-35,30),ylim=c(-40,45))
 
-set.seed(0)
-tsne.c<-Rtsne(all.dists2.c, is_distance=TRUE)#, perplexity = 0.9)
+    # MNN corrected.
+    Xmnn <- mnnCorrect(B1, B2, k=20, sigma=1, cos.norm=FALSE, svd.dim=2)
+    corre <- cbind(Xmnn$corrected[[1]],Xmnn$corrected[[2]])
+    all.dists2.c <- as.matrix(dist(t(corre)))
 
-png(file="mnn.png",width=900,height=700)
-par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
-plot(tsne.c$Y[1:N,1],tsne.c$Y[1:N,2], pch=16,cex=2.5,col=alpha((clustlabels[1:N]),0.6),main="MNN corrected",xlim=c(-30,40),ylim=c(-45,45),xlab="tSNE 1",ylab="tSNE 2")
-points(tsne.c$Y[(N+1):(2*N),1],tsne.c$Y[(N+1):(2*N),2], pch=2,cex=3.5,col=(clustlabels[(N+1):(2*N)]))
-dev.off()
+    set.seed(0)
+    tsne.c <- Rtsne(all.dists2.c, is_distance=TRUE)#, perplexity = 0.9)
+    plotFUN(paste0("figs/", prefix, "mnn.png"), Y=tsne.c$Y, batch.id=batch.id, col=clust.cols, main="MNN corrected",xlim=c(-30,40),ylim=c(-45,45))
 
-#############limma
-library(limma)
-Xlm <- removeBatchEffect(raw.all, factor(colnames(raw.all)))
-all.dists2.lm <- as.matrix(dist(t(Xlm)))
+    # limma.
+    Xlm <- removeBatchEffect(raw.all, factor(batch.id))
+    all.dists2.lm <- as.matrix(dist(t(Xlm)))
 
-set.seed(0)
-tsne.lm<-Rtsne(all.dists2.lm, is_distance=TRUE)#, perplexity = 0.9)
-png(file="lmfit.png",width=900,height=700)
-par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
-plot(tsne.lm$Y[1:N,1],tsne.lm$Y[1:N,2], pch=16,cex=2.5,col=alpha((clustlabels[1:N]),0.6),main="limma corrected",xlim=c(-30,30),ylim=c(-35,45),xlab="tSNE 1",ylab="tSNE 2")
-points(tsne.lm$Y[(N+1):(2*N),1],tsne.lm$Y[(N+1):(2*N),2], pch=2,cex=3.5,col=(clustlabels[(N+1):(2*N)]))
-dev.off()
+    set.seed(0)
+    tsne.lm <- Rtsne(all.dists2.lm, is_distance=TRUE)#, perplexity = 0.9)
+    plotFUN(paste0("figs/", prefix, "lmfit.png"), Y=tsne.lm$Y, batch.id=batch.id, col=clust.cols, main="limma corrected",xlim=c(-30,30),ylim=c(-35,45))
 
-####ComBat correction
-library(sva)
-Z <- colnames(raw.all)
-#mod<-model.matrix(~X)
-cleandat <- ComBat(raw.all,Z,mod=NULL,prior.plots = FALSE)
-all.dists.combat <- as.matrix(dist(t(cleandat)))
-set.seed(0)
-tsne.combat<-Rtsne(all.dists.combat, is_distance=TRUE)
+    # ComBat.
+    cleandat <- ComBat(raw.all, factor(batch.id), mod=NULL, prior.plots = FALSE)
+    all.dists.combat <- as.matrix(dist(t(cleandat)))
+    
+    set.seed(0)
+    tsne.combat<-Rtsne(all.dists.combat, is_distance=TRUE)
+    plotFUN(paste0("figs/", prefix, "combat.png"), Y=tsne.combat$Y, batch.id=batch.id, col=clust.cols, main="ComBat corrected",xlim=c(-35,30),ylim=c(-35,30))
+}
 
-png(file="combat.png",width=900,height=700)
-par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
-plot(tsne.combat$Y[1:N,1],tsne.combat$Y[1:N,2], pch=16,cex=2.5,col=alpha((clustlabels[1:N]),0.6),main="ComBat corrected",xlim=c(-35,30),ylim=c(-35,30),xlab="tSNE 1",ylab="tSNE 2")
-points(tsne.combat$Y[(N+1):(2*N),1],tsne.combat$Y[(N+1):(2*N),2], pch=2,cex=3.5,col=(clustlabels[(N+1):(2*N)]))
-dev.off()
-#############legend
+##########################################
+# Printing out the legend 
+
 png(file="leg.png",width=900,height=700)
-par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
-plot(tsne.c$Y[1,1],tsne.c$Y[1,2], pch=16,cex=2.5,col=alpha((clustlabels[1]),0.6),main="MNN corrected",xlim=c(-40,40),ylim=c(-45,45),xlab="tSNE 1",ylab="tSNE 2")
-#points(tsne.c$Y[(N+1):(2*N),1],tsne.c$Y[(N+1):(2*N),2], pch=2,cex=3.5,col=(clustlabels[(N+1):(2*N)]))
-leg.txt1=c("Cell type 1","Celltype 2","Celltype 3")
-leg.txt2=c("Batch1","Batch2")
-legend("topleft", legend = leg.txt1, col = c("brown1","dark green","blue") , pch = 15,
-       cex = 2.7,bty = "n")   #, trace = TRUE)
-legend(x=-42,y=25, legend = leg.txt2, col = "black" , pch = c(16,2),
-       cex = 2.7,bty = "n")   #, trace = TRUE)
+plot.new()
+legend("topleft", legend = c("Cell type 1", "Cell type 2", "Cell type 3", "Batch 1", "Batch 2"),
+       col = c("brown1", "dark green", "blue", "black", "black"), 
+       pch = c(15, 15, 15, 16, 2),
+       cex = 2.7,bty = "n")   
 dev.off()
--- Simulations2/simulateBatches.R
+++ Simulations2/simulateBatches.R
@@ -1,75 +1,89 @@
+# This script generates some (highly synthetic!) expression data with a batch effect 
+# and uneven population composition between batches.
 
-this.dir <- dirname(parent.frame(2)$ofile)
-setwd(this.dir)
+ncells <- 1000  # Number of cells
 
-N <- 1000  #no. cells
+# Our simulation involves three cell types/components.
+# Cells are distributed according to a bivariate normal in a 2-D biological subspace. 
+# Each cell type has a different x/y center and a different SD.
 
-###############Batch1
-### Set proportions of each of the three cell types in batch 1, and center and variance of each of the Gaussians in 2D 
-components <- sample(1:3,prob=c(0.3,0.5,0.2),size=N,replace=TRUE)
 xmus <- c(0,3,10)
 xsds <- c(1,0.1,1)
-
 ymus <- c(2,5,1)
 ysds <- c(1,0.1,1)
 
-########Sample N cells from Gaussian dist. according to the components (batch1)
-samples1<-matrix(0,nrow=N,ncol=2)
+#####################################
+# Generating data for batch 1, with a given proportion of cells in each component. 
+
+prop1 <- c(0.3,0.5,0.2)
+comp1 <- sample(1:3, prob=prop1, size=ncells, replace=TRUE)
+
+# Sampling locations for cells in each component.
 set.seed(0)
-samples1[,1] <- rnorm(n=N,mean=xmus[components],sd=xsds[components])
-samples1[,2] <- rnorm(n=N,mean=ymus[components],sd=ysds[components])
+samples1 <- cbind(rnorm(n=ncells, mean=xmus[comp1],sd=xsds[comp1]),
+                  rnorm(n=ncells, mean=ymus[comp1],sd=ysds[comp1]))
+    
+# Plot the true cluster locations for batch 1
+ref.cols <- c("blue", "brown1", "dark green")
+clust1 <- ref.cols[comp1]
+plot(samples1, pch=16, cex=1.5, col=clust1, main=paste0("Batch 1"))
 
-########### Get the true cluter labels for batch 1
-clust1<-components
-clust1[components==1]<-"blue"
-clust1[components==2]<-"brown1"
-clust1[components==3]<-"dark green"
-par(mfrow=c(1,1))
-plot(samples1, pch=16,cex=1.5,col=(clust1))
-############ Projection to D dimensional space
-D<-100
+# Random projection to D dimensional space, to mimic high-dimensional expression data.
+ngenes <- 100
 set.seed(0)
-R1D<-matrix(rnorm(D*N),  nrow=D, ncol=2)
+proj <- matrix(rnorm(ngenes*ncells), nrow=ngenes, ncol=2)
+A1 <- samples1 %*% t(proj)
 
-A1<-as.matrix(samples1) %*% t(as.matrix(R1D))
-#######Add normal noise
-rawd1 <- matrix(rnorm(D*N), ncol=D, nrow=N)
-A1<-as.data.frame(A1 + rawd1)
+# Add normally distributed noise.
+A1 <- A1 + rnorm(ngenes*ncells)
+rownames(A1) <- paste0("Cell", seq_len(ncells), "-1")
+colnames(A1) <- paste0("Gene", seq_len(ngenes))
 
-row.names(A1)<-c(1:nrow(A1))
-colnames(A1)<-c(1:D)
-###############Batch2
-### Set proportions of each of the three cell types in batch 2
-components <- sample(1:3,prob=c(0.65,0.3,0.05),size=N,replace=TRUE) 
+#####################################
 
-########Sample N cells from Gaussian dist. according to the components (batch2)
-samples2<-matrix(0,nrow=N,ncol=2)
-set.seed(0)
-samples2[,1] <- rnorm(n=N,mean=xmus[components],sd=xsds[components])
-samples2[,2] <- rnorm(n=N,mean=ymus[components],sd=ysds[components])
+outmat <- outcol <- list()
+for (easy in c(FALSE, TRUE)) { 
+    if (easy) { 
+        prop2 <- prop1
+    } else {
+        prop2 <- c(0.65,0.3,0.05)
+    }
+    
+    # Setting proportions of each of the three cell types in batch 2.
+    comp2 <- sample(1:3, prob=prop2, size=ncells, replace=TRUE) 
 
-########################### Get the true cluter labels for batch 2
-clust2<-components
-clust2[components==1]<-"blue"
-clust2[components==2]<-"brown1"
-clust2[components==3]<-"dark green"
-par(mfrow=c(1,1))
-plot(samples2, pch=16,cex=1.5,col=(clust2))
+    # Sampling locations for cells in each component.  
+    set.seed(0)
+    samples2 <- cbind(rnorm(n=ncells, mean=xmus[comp2], sd=xsds[comp2]),
+                      rnorm(n=ncells, mean=ymus[comp2], sd=ysds[comp2]))
+    
+    # Plot the true cluter labels for batch 2.
+    clust2 <- ref.cols[comp2]
+    plot(samples2, pch=16, cex=1.5, col=clust2, main=paste("Batch 2,", ifelse(easy, "same", "different"), "comp."))
+    
+    # Random projection, followed by adding batch effects and random noise.
+    A2 <- samples2 %*% t(proj) 
+    A2 <- A2 + matrix(rep(rnorm(ngenes), each=ncells), ncol=ngenes) # gene-specific batch effect (genes are columns)
+    A2 <- A2 + rnorm(ngenes*ncells) # noise
+    rownames(A2) <- paste0("Cell", seq_len(ncells), "-2")
+    colnames(A2) <- paste0("Gene", seq_len(ngenes))
+    
+    # Assigning batches and colours.
+    if (easy) {
+        outmat$ii <- A2
+        outcol$ii <- clust2
+    } else {
+        outmat$i <- A2
+        outcol$i <- clust2
+    }
+}
+    
+#####################################
+# Saving data objects for later use.
 
-############ Projection to D dimensional space
-A2<-as.matrix(samples2) %*% t(as.matrix(R1D))
-
-T2<-matrix(0,D,D)
-diag(T2)<-1
-
-####Add normal noise
-rawd2 <- matrix(rep(rnorm(1*D),each=N), nrow=N, ncol=D ) + matrix(rnorm(N*D), nrow=N, ncol=D) 
-A2<-as.data.frame(A2 %*% T2+  rawd2 )
-row.names(A2)<-c(1:nrow(A2))
-colnames(A2)<-c(1:D)
-
-##########save batches and celltype lables 
-B2<-as.data.frame(t(A2))
-B1<-as.data.frame(t(A1))
-save(file="Sim.RData",B1,B2,clust1,clust2)
-
+B1 <- t(A1)
+B2i <- t(outmat$i)
+B2ii <- t(outmat$ii)
+clust2i <- t(outcol$i)
+clust2ii <- t(outcol$ii)
+save(file="Sim.RData", B1, B2i, B2ii, clust1, clust2i, clust2ii)
