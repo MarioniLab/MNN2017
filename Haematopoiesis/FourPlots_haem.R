bae625c9f083bd23d07cbe1efd98cd4f4d042d74
Aaron Lun <alun@wehi.edu.au>
2017-10-14 13:40:09 +0100

    Cleaned up haematopoesis analysis code.

--- Haematopoiesis/FourPlots_haem.R
+++ Haematopoiesis/FourPlots_haem.R
@@ -1,106 +1,139 @@
-#Batch correction and t-SNA plots of haematopoietic data sets as in Fig.3
+# Batch correction and t-SNE plots of haematopoietic data sets, as in Figure 3.
+
 library(scran)
-require(WGCNA)
-this.dir <- dirname(parent.frame(2)$ofile)
-setwd(this.dir)
+dir.create("results", showWarning=FALSE)
 
-load("logdataFandA_all.RData") #load the output of "preparedata.R"
-setwd(paste0(this.dir,"/results"))
+# Load the output of "preparedata.R".
+load("logdataFandA_all.RData") 
+raw.all <- cbind(logDataF3, logDataA3)
+first.batch <- rep(c(TRUE, FALSE), c(ncol(logDataF3), ncol(logDataA3)))
 
-raw.all<-cbind(logdataF3,logdataA3)
-colnames(raw.all)<-c(rep(1,dim(logdataF3)[2]),rep(2,dim(logdataA3)[2]))
-celltypeF<-colnames(logdataF3)
-celltypeA<-substr( (colnames(logdataA3)),start = 1,stop = 3)  
-
-##organize cell type labels
-celltypes<-c(celltypeF,celltypeA)
+# Organize cell type labels.
+celltypes <- c(colnames(logDataF3), colnames(logDataA3))
 celltypes[celltypes=="other"]<- "Unsorted"
 celltypes[celltypes=="ERY"]<- "MEP"
-allcolors<-celltypes
-allcolors[celltypes=="MPP"]<-"blue"
-allcolors[celltypes=="LTH"]<-"dodgerblue"
-allcolors[celltypes=="LMP"]<-"light blue"
-allcolors[celltypes=="HSP"]<-"cyan"
-allcolors[celltypes=="Unsorted"]<-"grey"
-allcolors[celltypes=="MEP"]<-"red" 
-#allcolors[celltypes=="ERY"]<-"red"
-allcolors[celltypes=="GMP"]<-"dark green"
-allcolors[celltypes=="CMP"]<-"magenta"
 
+# Adding colours.
+color.legend <- c(MEP="red", GMP="dark green", CMP="magenta", HSPC="cyan", LTHSC="dodgerblue", MPP="blue", LMP="light blue", Unsorted="grey")
+allcolors <- color.legend[celltypes]
 
-N<-c(length(celltypeF),length(celltypeF)+length(celltypeA))
+plotFUN <- function(fname, Y, subset=NULL, ...) {
+    if (is.null(subset)) {
+        subset <- seq_len(nrow(Y))
+    }
+    png(fname,width=900,height=700)
+    par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
+    plot(Y[,1], Y[,2], cex=2,
+         pch=ifelse(first.batch, 21, 1)[subset], 
+         col=ifelse(first.batch, "black", allcolors)[subset],
+         bg=allcolors[subset], ...,  xlab="tSNE 1",ylab="tSNE 2")
+    dev.off()
+}
 
-######################Raw (uncorrected data)
-all.dists2.unc <- as.matrix(dist(t(raw.all)))
+# Only keeping common cell types for PCA.
+pca.retain <- celltypes %in% c("MEP", "GMP", "CMP") 
 
+######################
+# No correction.
+
+X.unc <- raw.all
+t.unc <- t(X.unc)
+
+# Generating a t-SNE plot.
 require(Rtsne)
-par(mfrow=c(1,1))
-
 set.seed(0)
-tsne.unc<-Rtsne(all.dists2.unc, is_distance=TRUE)#, perplexity = 90)
-#tsne.unc<-Rtsne(t(Xmnn$raw))#, perplexity = 0.9)
+all.dists.unc <- as.matrix(dist(t.unc))
+tsne.unc <- Rtsne(all.dists.unc, is_distance=TRUE)#, perplexity = 90)
+plotFUN("results/uncFA.png", tsne.unc$Y, main="Uncorrected")
 
-png(file="uncFA.png",width=900,height=700)
-par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
-plot(tsne.unc$Y[1:N[1],1],tsne.unc$Y[1:N[1],2], pch=21,col="black",cex=2,bg=allcolors[1:N[1]],main="Uncorrected",xlim=c(-35,35),ylim=c(-35,40),xlab="tSNE 1",ylab="tSNE 2")
-points(tsne.unc$Y[(N[1]+1):N[2],1],tsne.unc$Y[(N[1]+1):N[2],2], pch=1,cex=2,col=allcolors[(N[1]+1):N[2]])
-dev.off()
+rm(all.dists.unc)
+gc()
 
-######################## MNN corrected
-row.names(logdataF3)<-row.names(logdataA3)
-Xmnn<-mnnCorrect(logdataF3,logdataA3,inquiry.genes=row.names(logdataF3), hvg.genes=row.names(logdataF3), k=20, sigma=0.01, cos.norm=TRUE,svd.dim=2)
+# Generating a PCA plot.
+pca.unc <- prcomp(t.unc[pca.retain,], rank=2)
+plotFUN("results/pca_raw.png", pca.unc$x, subset=pca.retain, main="Uncorrected", ylim=c(-0.1, max(pca.unc$x[,2])))
 
-corre<-cbind(Xmnn$corrected[[1]],Xmnn$corrected[[2]])
-all.dists2.c <- as.matrix(dist(t(corre)))
+rm(t.unc)
+gc()
 
+######################## 
+# Performing the correction with MNN (turned down the sigma to improve mixing).
+
+mnn.out <- mnnCorrect(logDataF3, logDataA3, k=20, sigma=0.01, cos.norm=TRUE, svd.dim=2)
+X.mnn <- cbind(mnn.out$corrected[[1]], mnn.out$corrected[[2]])
+t.mnn <- t(X.mnn)
+
+# Generating a t-SNE plot.
 set.seed(0)
-tsne.c<-Rtsne(all.dists2.c, is_distance=TRUE)#, perplexity = 90)
-png(file="mnnFA.png",width=900,height=700)
-par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
-plot(tsne.c$Y[1:N[1],1],tsne.c$Y[1:N[1],2],pch=21,col="black",cex=2.5,bg=allcolors[1:N[1]],main="MNN corrected",xlim=c(-30,35),ylim=c(-45,40),xlab="tSNE 1",ylab="tSNE 2")
-points(tsne.c$Y[(N[1]+1):N[2],1],tsne.c$Y[(N[1]+1):N[2],2], pch=1,cex=2,col=allcolors[(N[1]+1):N[2]])
-dev.off()
+all.dists.mnn <- as.matrix(dist(t.mnn))
+tsne.mnn <- Rtsne(all.dists.mnn, is_distance=TRUE)#, perplexity = 90)
+plotFUN("results/mnnFA.png", tsne.mnn$Y, main="MNN")
 
-##### legend
-leg.txt2<-c("MEP", "GMP" ,"CMP", "HSP","LTH" ,   "MPP" , "LMP",  "Unsorted")
-forcoloringleg<-c("red", "dark green","magenta", "cyan", "dodgerblue","blue","light blue","grey" )
+rm(all.dists.mnn)
+gc()
 
-png(file="legFA.png",width=900,height=700)
-par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
-plot(tsne.c$Y[1,1],tsne.c$Y[1,2], pch=1,cex=2,col=allcolors[1],xlim=c(-20,-12),ylim=c(0,3))
-leg.txt<-unique(celltypes)
-legend(x=-20, y=3, legend =leg.txt2, pch=21, cex = 2.5, pt.bg=forcoloringleg ,bty = "n")   #, trace = TRUE)
-legend(x=-18,y=3, legend =leg.txt2[1:3], col = forcoloringleg[1:3], pch = 1,cex = 2.5,bty = "n",lwd = 3,lty=0) 
-dev.off()
+# Generating a PCA plot.
+pca.mnn <- prcomp(t.mnn[pca.retain,], rank=2)
+plotFUN("results/pca_mnn.png", pca.mnn$x, subset=pca.retain, main="MNN")
 
-#################limma
+rm(t.mnn)
+gc()
+
+######################## 
+# Performing the correction with limma.
+
 library(limma)
-Xlm <- removeBatchEffect(raw.all, factor(colnames(raw.all)))
-all.dists2.lm <- as.matrix(dist(t(Xlm)))
+X.lm <- removeBatchEffect(raw.all, factor(first.batch))
+t.lm <- t(X.lm)
 
+# Generating a t-SNE plot.
 set.seed(0)
-tsne.lm<-Rtsne(all.dists2.lm, is_distance=TRUE)#, perplexity = 90)
-png(file="lmfitFA.png",width=900,height=700)
-par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
-plot(tsne.lm$Y[1:N[1],1],tsne.lm$Y[1:N[1],2], pch=21,col="black",cex=2,bg=allcolors[1:N[1]],main="limma corrected",xlim=c(-40,35),ylim=c(-40,45),xlab="tSNE 1",ylab="tSNE 2")
-points(tsne.lm$Y[(N[1]+1):N[2],1],tsne.lm$Y[(N[1]+1):N[2],2], pch=1,cex=2,col=allcolors[(N[1]+1):N[2]])
-dev.off()
+all.dists.lm <- as.matrix(dist(t.lm))
+tsne.lm <- Rtsne(all.dists.lm, is_distance=TRUE)#, perplexity = 90)
+plotFUN("results/lmfitFA.png", tsne.lm$Y, main="limma")
 
-####ComBat 
+rm(all.dists.lm)
+gc()
+
+# Generating a PCA plot.
+pca.lm <- prcomp(t.lm[pca.retain,], rank=2)
+plotFUN("results/pca_lm.png", pca.lm$x, subset=pca.retain, main="limma", ylim=c(min(pca.lm$x[,2]), 0.05))
+
+rm(t.lm)
+gc()
+
+######################## 
+# Performing the correction with ComBat.
 
 library(sva)
-Z <- colnames(raw.all)
-cleandat.combat <- ComBat(raw.all,Z,mod=NULL,prior.plots = FALSE)
-all.dists.combat <- as.matrix(dist(t(cleandat.combat)))
+Z <- factor(first.batch)
+X.combat <- ComBat(raw.all,Z,mod=NULL,prior.plots = FALSE)
+t.combat <- t(X.combat)
+
+# Generating a t-SNE plot.
 set.seed(0)
+all.dists.combat <- as.matrix(dist(t.combat))
 tsne.combat<-Rtsne(all.dists.combat, is_distance=TRUE)#,perplexity = 90)
+plotFUN("results/combatFA.png", tsne.combat$Y, main="ComBat")
 
+rm(all.dists.combat)
+gc()
 
-png(file="combatFA.png",width=900,height=700)
-par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
-plot(tsne.combat$Y[1:N[1],1],tsne.combat$Y[1:N[1],2], pch=21,col="black",cex=2,bg=allcolors[1:N[1]],main="ComBat corrected",xlim=c(-30,40),ylim=c(-45,35),xlab="tSNE 1",ylab="tSNE 2")
-points(tsne.combat$Y[(N[1]+1):N[2],1],tsne.combat$Y[(N[1]+1):N[2],2], pch=1,cex=2,col=allcolors[(N[1]+1):N[2]])
+# Generating a PCA plot.
+pca.combat <- prcomp(t.combat[pca.retain,], rank=2)
+plotFUN("results/pca_com.png", pca.combat$x, subset=pca.retain, main="ComBat", ylim=c(min(pca.combat$x[,2]), 0.05))
+
+rm(t.combat)
+gc()
+
+######################## 
+# Making the legend (using PDF for better resolution).
+
+pdf(file="results/legFA.pdf", width=7, height=7)
+plot(0,0,type="n", bty="n", axes=FALSE, xlab="", ylab="")
+legend(x=-1, y=1, legend=names(color.legend), pch=21, cex=2.5, col="black", pt.bg=color.legend, bty="n")
+legend(x=0, y=1, legend=names(color.legend)[1:3], col = color.legend[1:3], pch = 1, cex = 2.5, bty = "n", lwd = 3, lty=0) 
 dev.off()
 
-#save corrected data
-save(list = ls(), file = "FAcorrected.RData")
+######################## 
+# END
--- Haematopoiesis/preparedata.R
+++ Haematopoiesis/preparedata.R
@@ -1,101 +1,91 @@
-#checked
-#SET WORKING DIRECTORY ######
+# This script prepares data for the haematopoiesis analysis.
+# It involves two batches of publicly available data.
 
-this.dir <- dirname(parent.frame(2)$ofile)
-setwd(this.dir)
+##########################################
+##########################################
 
-############naive size factor corection
-c_by_sizefactor<-function(X){
-  G<-dim(X)[1]
-  n<-dim(X)[2]
-  cellnorm<-colSums(X)
-  cellnorm.nonzero<-cellnorm
-  cellnorm.nonzero[which(cellnorm==0)]<-1
-  Y<-X/matrix(rep(cellnorm.nonzero,each=G),G,n)
-}
-###########
-library(utils)
-#download and read the counts and meta data of Nestorowa et al. 2016
-download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE81682&format=file&file=GSE81682%5FHTSeq%5Fcounts%2Etxt%2Egz", "GSE81682_HTSeq_counts.txt")
-dataF<-read.table("GSE81682_HTSeq_counts.txt", head=T)
-featuresF<-dataF[,1]
-dataF<-dataF[,-1]
-row.names(dataF)<-featuresF
+# Download and read the counts, metadata of Nestorowa et al. 2016
+fname <- "GSE81682_HTSeq_counts.txt.gz"
+if (!file.exists(fname)) { download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE81682&format=file&file=GSE81682%5FHTSeq%5Fcounts%2Etxt%2Egz", fname) }
+dataF <- read.table(fname, header=TRUE, row.names=1, check.names=FALSE)
 dim(dataF)
 
-download.file("http://blood.stemcells.cam.ac.uk/data/all_cell_types.txt", "metaF.txt")
-metaF<-read.table("metaF.txt",stringsAsFactors = F, head=T)
-metainds<-match(row.names(metaF),colnames(dataF))
-narow<-which(is.na(metainds))
-metainds<-metainds[-narow]
-metaF<-metaF[-narow,]
+fname <- "metaF.txt"
+if (!file.exists(fname)) { download.file("http://blood.stemcells.cam.ac.uk/data/all_cell_types.txt", fname) }
+metaF <- read.table(fname, stringsAsFactors = FALSE, header=TRUE, check.names=FALSE)
+metainds <- match(colnames(dataF), rownames(metaF))
+missing.meta <- is.na(metainds)
+metaF <- metaF[metainds,] # This will contain NA's... which is okay, at this point, to preserve length.
 
-metatypeF<-vector(mode="character",length=dim(metaF)[1])
-for (i in 1:dim(metaF)[1]) {
-whichcol<-which(metaF[i,]==1)
-  if (length(whichcol) >0) {
-  metatypeF[i]<-substr(colnames(metaF)[whichcol[1]],start=1,stop=3) 
-  }
-  else{metatypeF[i]<-"other"
-  }
+# Defining the cell type based on the metadata.
+metatypeF <- rep("other", nrow(metaF))
+for (col in rev(colnames(metaF))) { # reverse, so earlier columns end up overwriting later ones.
+    chosen <- metaF[,col]==1
+    metatypeF[chosen] <- sub("[0-9]?_.*", "", col)
 }
+metatypeF[metatypeF=="ESLAM"] <- "HSPC"
 
-metatypeF[metatypeF=="ESL"]<-"HSP"
-leftouts<- setdiff(c(1:ncol(dataF)),metainds )
-metainds<-c(metainds,leftouts)
-metatypeF<-c(metatypeF,substr(colnames(dataF)[leftouts], start=1,stop=3))
-dataF<-dataF[,metainds]
-metatypeF[metatypeF=="LT."]<-"LTH"
-metatypeF[metatypeF=="Pro"]<-"other"
+# Filling in metadata from the cell sorting label, if metadata was missing.
+metatypeF[missing.meta] <- sub("_.*", "", colnames(dataF)[missing.meta])
+metatypeF[metatypeF=="LT-HSC"] <- "LTHSC"
+metatypeF[metatypeF=="Prog"] <- "other"
 colnames(dataF)<-metatypeF
 
-#download and read the counts and meta data of Paul et al. 2015
-download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE72857&format=file&file=GSE72857%5Fumitab%2Etxt%2Egz","umitab_Amit.txt")
-dataA<-read.table("umitab_Amit.txt", head=T)
-metaA<-read.csv2("MAP.csv",sep=",",stringsAsFactors = F, head=T)
-row.names(metaA)<-metaA[,1]
+##########################################
+##########################################
+
+# Download and read the counts and meta data of Paul et al. 2015
+fname <- "umitab_Amit.txt.gz"
+if (!file.exists(fname)) { download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE72857&format=file&file=GSE72857%5Fumitab%2Etxt%2Egz", fname) }
+dataA <- read.table(fname, header=TRUE, row.names=1)
+metaA <- read.csv2("MAP.csv",sep=",",stringsAsFactors = FALSE, head=TRUE, row.names=1)
 dim(dataA)
 
-metainds<-match(row.names(metaA),colnames(dataA))
-dataA<-dataA[,metainds]
+# Only selecting cells that are in the metadata.
+metainds <- match(rownames(metaA), colnames(dataA))
+dataA <- dataA[,metainds]
 
-#organize cell type labels
-metatypeA<-vector(mode="character",length=dim(metaA)[1])
-metatypeA[metaA[,2]<7]<-"ERY"
-metatypeA[metaA[,2]>6 &metaA[,2]<12]<-"CMP"
-metatypeA[metaA[,2]>11]<-"GMP"
-Amit.celltypes<-metatypeA
+# Organizing cell type labels.
+metatypeA <- character(nrow(metaA))
+metatypeA[metaA[,1]<7] <- "ERY"
+metatypeA[metaA[,1]>6 & metaA[,1]<12] <- "CMP"
+metatypeA[metaA[,1]>11] <- "GMP"
+colnames(dataA) <- metatypeA
 
-colnames(dataA)<-unlist(Amit.celltypes)
+##########################################
+##########################################
 
-##download list of highly variable genes identified by Nestrowa et al. 2016
-download.file("http://blood.stemcells.cam.ac.uk/data/coordinates_gene_counts_flow_cytometry.txt.gz","coordinates_gene_counts_flow_cytometry.txt")
-TFs <- read.table("coordinates_gene_counts_flow_cytometry.txt",
-                  nrows =  1)
-###### organize and match gene names between the two data sets
-source("../SomeFuncs/match_gene_names.R")
-features<-as.character(unlist(TFs))
-features<-features[28:4800]
-out<-matchgenenamestoENS(features,idknown = TRUE)
-dataF2<-dataF[out$ensembl_gene_id,]
-dataA2<-dataA[out$mgi_symbol,]
+# Download list of highly variable genes identified by Nestrowa et al. 2016
+fname <- "coordinates_gene_counts_flow_cytometry.txt.gz"
+if (!file.exists(fname)) { download.file("http://blood.stemcells.cam.ac.uk/data/coordinates_gene_counts_flow_cytometry.txt.gz", fname) }
+TFs <- read.table(fname, nrows=1, stringsAsFactors=FALSE)
+features <- as.character(unlist(TFs))
+features <- features[grep("ENSMUS", features)]
 
-colnames(dataA2)<-colnames(dataA)
-##tidy up 
- narow<-which(is.na(dataF2[,2])) #there was a narow!
-if (length(narow) >0) {
- dataF2<-dataF2[-narow,]
- dataA2<-dataA2[-narow,]
-}
- narow<-which(is.na(dataA2[,2]))  #there were several narow!
- if (length(narow) >0) {
- dataF2<-dataF2[-narow,]
- dataA2<-dataA2[-narow,]
- }
-####size factor corrections + log transform
-dataF2<-(c_by_sizefactor(dataF2)) 
-dataA2<-(c_by_sizefactor(dataA2))  # 
-logdataF3<-(log(1+dataF2))
-logdataA3<-(log(1+dataA2))
+# Pull down IDs from BioMaRt.
+library(biomaRt)
+mart <- useMart("ensembl", dataset = "mmusculus_gene_ensembl", host="www.ensembl.org" )
+out <- getBM(attributes = c("ensembl_gene_id", "mgi_symbol"), values = features, mart = mart,filters = "ensembl_gene_id")
 
-save(list=c("logdataA3","logdataF3"),file="logdataFandA_all.RData")
+# Select features that are HVGs _and_ present in both data sets.
+mF <- match(out$ensembl_gene_id, rownames(dataF))
+mA <- pmatch(out$mgi_symbol, rownames(dataA)) # partial, due to use of concatenated gene symbols.
+keep <- !is.na(mF) & !is.na(mA)
+
+dataA2 <- dataA[mA[keep],]
+dataF2 <- dataF[mF[keep],]
+rownames(dataA2) <- rownames(dataF2)
+
+# Apply library size normalization and perform log-transformation.
+libF <- colSums(dataF2)
+dataF2 <- t(t(dataF2)/libF)
+logDataF3 <- as.matrix(log(1 + dataF2))
+
+libA <- colSums(dataA2)
+dataA2 <- t(t(dataA2)/libA)
+logDataA3 <- as.matrix(log(1 + dataA2))
+
+save(logDataA3, logDataF3, file="logdataFandA_all.RData")
+
+###########
+# END
