05f38e1a3a27c51b91956a57d295370bb191cffc
Aaron Lun <alun@wehi.edu.au>
2017-10-27 19:51:31 +0100

    Further prettification of PCA plots.

--- Haematopoiesis/plotCorrections.R
+++ Haematopoiesis/plotCorrections.R
@@ -5,33 +5,30 @@ dir.create("results", showWarning=FALSE)
 
 # Load the output of "preparedata.R".
 load("logdataFandA_all.RData") 
+colnames(logDataF3)[colnames(logDataF3)=="other"] <- "Unsorted"
+colnames(logDataA3)[colnames(logDataA3)=="ERY"] <- "MEP"
 raw.all <- cbind(logDataF3, logDataA3)
 first.batch <- rep(c(TRUE, FALSE), c(ncol(logDataF3), ncol(logDataA3)))
 
-# Organize cell type labels.
-celltypes <- c(colnames(logDataF3), colnames(logDataA3))
-celltypes[celltypes=="other"]<- "Unsorted"
-celltypes[celltypes=="ERY"]<- "MEP"
-
 # Adding colours.
 base.color <- "grey"
-color.legend <- c(MEP="red",
-                  GMP="blue",
-                  CMP="goldenrod", 
-                  HSPC=base.color, LTHSC=base.color, MPP=base.color, LMPP=base.color, Unsorted=base.color)
-colmat <- col2rgb(color.legend[celltypes])
-colmat[,!first.batch] <- colmat[,!first.batch]+200 # A lighter shade.
-colmat[colmat > 255] <- 255
-allcolors <- rgb(colmat[1,], colmat[2,], colmat[3,], maxColorValue=255)
+color.legendF <- c(MEP="red", GMP="blue", CMP="orange", 
+                   HSPC=base.color, LTHSC=base.color, MPP=base.color, LMPP=base.color, Unsorted=base.color)
+colmatF <- col2rgb(color.legendF)
+colmatA <- colmatF + 200 # A lighter shade.
+colmatA[colmatA > 255] <- 255
+color.legendA <- setNames(rgb(colmatA[1,], colmatA[2,], colmatA[3,], maxColorValue=255), names(color.legendF))
+allcolors <- c(color.legendF[colnames(logDataF3)], color.legendA[colnames(logDataA3)])
 
 # Making a plotting function.
 plotFUN <- function(fname, Y, subset=NULL, ..., xlab="tSNE 1",ylab="tSNE 2") {
     # Setting up the plotting device.
-    png(fname,width=900,height=700)
-    par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=2,cex.main=3,cex.lab=2.5)
+    pdf(fname,width=9,height=7)
+    par(mfrow=c(1,1),mar=c(6,6,4,2),cex.axis=1.2,cex.main=1.4,cex.lab=1.4)
 
     # Plot the first, smaller batch ON TOP OF the second, larger batch.
-    reorder <- rev(seq_len(nrow(Y)))
+    set.seed(0)
+    reorder <- sample(nrow(Y))
     plot(Y[reorder,1], Y[reorder,2], cex=2, pch=21, col="black", 
          bg=allcolors[reorder], ..., xlab=xlab, ylab=ylab) 
     dev.off()
@@ -48,14 +45,14 @@ t.unc <- t(X.unc)
 #set.seed(0)
 #all.dist.unc <- dist(t.unc)
 #tsne.unc <- Rtsne(all.dist.unc, is_distance=TRUE, perplexity = 30)
-#plotFUN("results/uncFA.png", tsne.unc$Y, main="Uncorrected")
-#plotFUN("results/uncFAb.png", tsne.unc$Y, main="Uncorrected", by.batch=TRUE)
+#plotFUN("results/uncFA.pdf", tsne.unc$Y, main="Uncorrected")
+#plotFUN("results/uncFAb.pdf", tsne.unc$Y, main="Uncorrected", by.batch=TRUE)
 #
 #gc()
 
 # Generating a PCA plot.
 pca.unc <- prcomp(t.unc, rank=2)
-plotFUN("results/pca_raw.png", pca.unc$x, main="Uncorrected", xlab="PC1", ylab="PC2")
+plotFUN("results/pca_raw.pdf", pca.unc$x, main="Uncorrected", xlab="PC1", ylab="PC2")
 
 rm(t.unc)
 gc()
@@ -71,14 +68,14 @@ t.mnn <- t(X.mnn)
 #set.seed(0)
 #all.dist.mnn <- as.matrix(dist(t.mnn))
 #tsne.mnn <- Rtsne(all.dist.mnn, is_distance=TRUE, perplexity = 30)
-#plotFUN("results/mnnFA.png", tsne.mnn$Y, main="MNN")
-#plotFUN("results/mnnFAb.png", tsne.mnn$Y, main="MNN", by.batch=TRUE)
+#plotFUN("results/mnnFA.pdf", tsne.mnn$Y, main="MNN")
+#plotFUN("results/mnnFAb.pdf", tsne.mnn$Y, main="MNN", by.batch=TRUE)
 #
 #gc()
 
 # Generating a PCA plot.
 pca.mnn <- prcomp(t.mnn, rank=2)
-plotFUN("results/pca_mnn.png", pca.mnn$x, main="MNN", xlab="PC1", ylab="PC2")
+plotFUN("results/pca_mnn.pdf", pca.mnn$x, main="MNN", xlab="PC1", ylab="PC2")
 
 rm(t.mnn)
 gc()
@@ -94,14 +91,14 @@ t.lm <- t(X.lm)
 #set.seed(0)
 #all.dist.lm <- as.matrix(dist(t.lm))
 #tsne.lm <- Rtsne(all.dist.lm, is_distance=TRUE, perplexity = 30)
-#plotFUN("results/lmfitFA.png", tsne.lm$Y, main="limma")
-#plotFUN("results/lmfitFAb.png", tsne.lm$Y, main="limma", by.batch=TRUE)
+#plotFUN("results/lmfitFA.pdf", tsne.lm$Y, main="limma")
+#plotFUN("results/lmfitFAb.pdf", tsne.lm$Y, main="limma", by.batch=TRUE)
 #
 #gc()
 
 # Generating a PCA plot.
 pca.lm <- prcomp(t.lm, rank=2)
-plotFUN("results/pca_lm.png", pca.lm$x, main="limma", xlab="PC1", ylab="PC2")
+plotFUN("results/pca_lm.pdf", pca.lm$x, main="limma", xlab="PC1", ylab="PC2")
 
 rm(t.lm)
 gc()
@@ -118,14 +115,14 @@ t.combat <- t(X.combat)
 #set.seed(0)
 #all.dists.combat <- as.matrix(dist(t.combat))
 #tsne.combat <- Rtsne(all.dists.combat, is_distance=TRUE, perplexity=30)
-#plotFUN("results/combatFA.png", tsne.combat$Y, main="ComBat")
-#plotFUN("results/combatFAb.png", tsne.combat$Y, main="ComBat", by.batch=TRUE)
+#plotFUN("results/combatFA.pdf", tsne.combat$Y, main="ComBat")
+#plotFUN("results/combatFAb.pdf", tsne.combat$Y, main="ComBat", by.batch=TRUE)
 #
 #gc()
 
 # Generating a PCA plot.
 pca.combat <- prcomp(t.combat, rank=2)
-plotFUN("results/pca_com.png", pca.combat$x, main="ComBat", xlab="PC1", ylab="PC2")
+plotFUN("results/pca_com.pdf", pca.combat$x, main="ComBat", xlab="PC1", ylab="PC2")
 
 rm(t.combat)
 gc()
@@ -136,7 +133,9 @@ gc()
 pdf(file="results/legFA.pdf", width=7, height=7)
 plot(0,0,type="n", bty="n", axes=FALSE, xlab="", ylab="")
 legend(x=-1, y=1, legend=names(color.legend), pch=21, cex=2.5, col="black", pt.bg=color.legend, bty="n")
-legend(x=0, y=1, legend=names(color.legend)[1:3], col = color.legend[1:3], pch = 1, cex = 2.5, bty = "n", lwd = 3, lty=0) 
+legend(x=0, y=1, legend=names(color.legend)[1:3], 
+       col = color.legend[1:3], 
+       pch = 21, cex = 2.5, bty = "n", lwd = 3, lty=0) 
 dev.off()
 
 ######################## 
