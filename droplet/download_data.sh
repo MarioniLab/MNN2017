1e8d92eeec83d835299d005319aca7b725f75e4f
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-16 14:30:33 +0100

    Added pre-processing code for the 4k T cell data.

--- droplet/download_data.sh
+++ droplet/download_data.sh
@@ -5,5 +5,15 @@ mkdir -p raw_data
 curl -L -o raw_data/68k.tar.gz http://cf.10xgenomics.com/samples/cell-exp/1.1.0/fresh_68k_pbmc_donor_a/fresh_68k_pbmc_donor_a_filtered_gene_bc_matrices.tar.gz
 cd raw_data
 tar -xvf 68k.tar.gz
+mv filtered_matrices_mex pbmc68k
 rm 68k.tar.gz
+cd -
+
+# Downloading the 4K T-cell dataset.
+
+curl -L -o raw_data/4k.tar.gz http://cf.10xgenomics.com/samples/cell-exp/2.1.0/t_4k/t_4k_filtered_gene_bc_matrices.tar.gz
+cd raw_data
+tar -xvf 4k.tar.gz
+mv filtered_gene_bc_matrices t4k
+rm 4k.tar.gz
 cd -
--- droplet/prepareData.R
+++ droplet/prepareData.R
@@ -5,7 +5,8 @@ library(DropletUtils)
 ##########################################
 ##########################################
 
-sce.68 <- read10xCounts("raw_data/filtered_matrices_mex/hg19/") 
+# Pre-processing the 68K PBMC dataset.
+sce.68 <- read10xCounts("raw_data/pbmc68k/hg19/") 
 
 library(org.Hs.eg.db)
 symb <- mapIds(org.Hs.eg.db, keys=rownames(sce.68), keytype="ENSEMBL", column="SYMBOL")
@@ -22,23 +23,52 @@ lowlib <- isOutlier(sce.68$scater_qc$all$log10_total_counts, type="lower", nmads
 lowfeat <- isOutlier(sce.68$scater_qc$all$log10_total_features_by_counts, type="lower", nmads=3)
 highmito <- isOutlier(sce.68$scater_qc$feature_control_Mt$pct_counts, type="higher", nmads=3)
 discard <- lowlib | lowfeat | highmito
-
+##summary(discard)
 sce.68 <- sce.68[,!discard]
 
 # Performing normalization, breaking the problem up into smaller blocks and subclustering within them.
 blocks <- rep(seq_len(10), length.out=ncol(sce.68))
-clusters <- quickCluster(sce.68, min.mean=0.1, block=blocks, method="igraph", BPPARAM=MulticoreParam(2))
+clusters <- quickCluster(sce.68, min.mean=0.1, block=blocks, method="igraph", block.BPPARAM=MulticoreParam(2))
 ##table(clusters)
 ##table(clusters, blocks)
+
 sce.68 <- computeSumFactors(sce.68, clusters=clusters, min.mean=0.1, BPPARAM=MulticoreParam(2))
 ##plot(sce.68$scater_qc$all$total_counts, sizeFactors(sce.68), log="xy")
-sce.68 <- normalize(sce.68)
 
-# Identifying (relatively) highly variable genes.
-fit <- trendVar(sce.68, use.spikes=FALSE, loess.args=list(span=0.1))
-##plot(fit$mean, fit$var)
-##curve(fit$trend(x), col="red",add=TRUE)
-dec.68 <- decomposeVar(fit=fit)
+saveRDS(file="sce.pbmc68k.rds", sce.68)
 
-saveRDS(file="dec68.rds", dec.68)
-saveRDS(file="sce68.rds", sce.68)
+# Cleaning out the memory.
+rm(list=ls())
+gc()
+
+##########################################
+##########################################
+
+# Pre-processing the 4K T-cell dataset.
+sce.4 <- read10xCounts("raw_data/t4k/GRCh38") 
+symb <- mapIds(org.Hs.eg.db, keys=rownames(sce.4), keytype="ENSEMBL", column="SYMBOL")
+rowData(sce.4)$Symbol <- symb
+
+# Adding locational annotation.
+library(EnsDb.Hsapiens.v86)
+loc <- mapIds(EnsDb.Hsapiens.v86, keys=rownames(sce.4), keytype="GENEID", column="SEQNAME")
+rowData(sce.4)$Chr <- loc
+
+# Brief quality control.
+sce.4 <- calculateQCMetrics(sce.4, compact=TRUE, feature_controls=list(Mt=which(loc=="MT")))
+lowlib <- isOutlier(sce.4$scater_qc$all$log10_total_counts, type="lower", nmads=3)
+lowfeat <- isOutlier(sce.4$scater_qc$all$log10_total_features_by_counts, type="lower", nmads=3)
+highmito <- isOutlier(sce.4$scater_qc$feature_control_Mt$pct_counts, type="higher", nmads=3)
+discard <- lowlib | lowfeat | highmito
+##summary(discard)
+
+sce.4 <- sce.4[,!discard]
+
+# Performing normalization.
+clusters <- quickCluster(sce.4, min.mean=0.1, method="igraph")
+##table(clusters)
+
+sce.4 <- computeSumFactors(sce.4, clusters=clusters, min.mean=0.1, BPPARAM=MulticoreParam(2))
+##plot(sce.4$scater_qc$all$total_counts, sizeFactors(sce.4), log="xy")
+
+saveRDS(file="sce.t4k.rds", sce.4)
